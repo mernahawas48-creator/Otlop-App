933365b475050a10f55553233e79de5b5ad02664
Mirna-Hawas <mernahawas48@gmail.com>
2026-09-20 17:04:17 +0300

    add

--- lib/features/auth/cubit/sign_up_cubit.dart
+++ lib/features/auth/cubit/sign_up_cubit.dart
@@ -1,32 +1,39 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:otlopapp/core/networking/api_consumer.dart';
-import 'package:meta/meta.dart';
+import 'package:otlopapp/core/networking/api_error_handler.dart';
+
 part 'sign_up_state.dart';
 
 class SignUpCubit extends Cubit<SignUpState> {
   final ApiConsumer apiConsumer;
+
   SignUpCubit({required this.apiConsumer}) : super(SignUpInitial());
-  signUp({
+
+  Future<void> signUp({
     required String email,
     required String password,
-    required String firstname,
-    required String lastname,
+    required String firstName,
+    required String lastName,
   }) async {
     emit(SignUpLoading());
+
     try {
-      Response response = await apiConsumer.post(
-        path: 'auth/sign-up',
+      await apiConsumer.post(
+        path: 'auth/register',
         data: {
           'email': email,
           'password': password,
-          'firstname': firstname,
-          'lastname': lastname,
+          'firstName': firstName,
+          'lastName': lastName,
         },
       );
+
       emit(SignUpSuccess());
+    } on DioException catch (e) {
+      emit(SignUpFailure(errorMessage: ApiErrorHandeler.handleError(e)));
     } catch (e) {
-      emit(SignUpFailure(message: e.toString()));
+      emit(SignUpFailure(errorMessage: e.toString()));
     }
   }
 }
---
+++ lib/features/auth/cubit/sign_up_state.dart
@@ -0,0 +1,15 @@
+part of 'sign_up_cubit.dart';
+
+sealed class SignUpState {}
+
+final class SignUpInitial extends SignUpState {}
+
+final class SignUpLoading extends SignUpState {}
+
+final class SignUpSuccess extends SignUpState {}
+
+final class SignUpFailure extends SignUpState {
+  SignUpFailure({required this.errorMessage});
+
+  final String errorMessage;
+}
